217966926bf44d4379c0b072af8b59a6590e36e9
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2025-11-01 14:49:46 +0500

    able to edit props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import { HikPlayer } from '@/components/HikPlayer.tsx';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input.tsx';
 
 function App() {
+  const [accessToken, setAccessToken] = useState('hcc.qDkUxx1RnfVspsSxZHfcU6SWjDCz6UzZ');
+  const [secretKey, setSecretKey] = useState('16071991aZ');
+  const [serialNumber, setSerialNumber] = useState('FU5068433');
+  const [domain, setDomain] = useState('https://isgp.hikcentralconnect.com');
+
   return (
     <div>
+      <div className='flex items-center justify-between gap-4 p-4'>
+        <Input
+          placeholder='Access token'
+          value={accessToken}
+          onChange={(e) => setAccessToken(e.target.value)}
+        />
+        <Input
+          placeholder='Secret key'
+          value={secretKey}
+          onChange={(e) => setSecretKey(e.target.value)}
+        />
+        <Input
+          placeholder='Serial number'
+          value={serialNumber}
+          onChange={(e) => setSerialNumber(e.target.value)}
+        />
+        <Input placeholder='domain' value={domain} onChange={(e) => setDomain(e.target.value)} />
+      </div>
       <HikPlayer
-        accessToken='hcc.qDkUxx1RnfVspsSxZHfcU6SWjDCz6UzZ'
-        secretKey='16071991aZ'
-        serialNumber='FU5068433'
+        accessToken={accessToken}
+        secretKey={secretKey}
+        serialNumber={serialNumber}
         channelNumber={1}
-        domain='https://isgp.hikcentralconnect.com'
+        domain={domain}
         mode='live'
         storageType='local'
         onError={(err) => console.error(err)}
